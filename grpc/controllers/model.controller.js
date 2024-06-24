58bb2666635909cc88972ceebf86563de4a835a4
Rui Gomes <yruigomes99@gmail.com>
2024-06-24 18:07:22 +0100

    add getModels to grpc and update jwt

--- grpc/controllers/model.controller.js
+++ grpc/controllers/model.controller.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose')
 
 const ModelCatalogue = require('../../models/modelCatalogue')
+const jwt = require('../../utils/jsonwebtoken')
 
 async function getModel(call, callback) {
     try {
         logger.debug(`Received gRPC call: ${JSON.stringify(call.request)}`)
+
+        const authHeader = call.metadata.get('authorization')[0]
+        const { id } = await jwt.decodeSessionToken(jwt.getTokenFromBearer(authHeader))
 
         const { model_id } = call.request
 
         let found = false
         let model = ''
         if (mongoose.Types.ObjectId.isValid(model_id)) {
-            const modelCatalogue = await ModelCatalogue.findOne({ _id: model_id, deleted: false }).populate('features').populate('dependencies')
+            const modelCatalogue = await ModelCatalogue.findOne({ _id: model_id, user_id: id, deleted: false }).populate('features').populate('dependencies')
             if (modelCatalogue) {
                 found = true
                 model = JSON.stringify(modelCatalogue)
@@ -24,6 +28,22 @@ async function getModel(call, callback) {
     }
 }
 
+async function getModels(call, callback) {
+    try {
+        logger.debug(`Received gRPC call: calling method gedModels`)
+
+        const authHeader = call.metadata.get('authorization')[0]
+        const { id } = await jwt.decodeSessionToken(jwt.getTokenFromBearer(authHeader))
+
+        const models = await ModelCatalogue.find({ user_id: id }, { _id: 1, name: 1, deleted: 1 })
+        callback(null, { models: models })
+    } catch (err) {
+        logger.error(err)
+        callback(null, { models: [] })
+    }
+}
+
 module.exports = {
-    getModel
+    getModel,
+    getModels
 }
